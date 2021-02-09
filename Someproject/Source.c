d8463a1eeff0816ecd3550744b67ed2e1edf8123
Николай <72332874+Nikolai-Zelensky@users.noreply.github.com>
2021-02-09 10:23:40 +0700

    Update Source.c

--- Someproject/Source.c
+++ Someproject/Source.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #define size 3
 
 struct Sportteam
@@ -13,18 +14,18 @@ struct Sportteam
 void some_function(struct Sportteam* team)
 {
 	struct Sportteam t[size];
-	for (short i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)
 	{
 		printf("Enter the team name:\n");
 		scanf("%s", t[i].name);
 		printf("Enter the city:\n");
 		scanf("%s", t[i].city);
 		printf("Enter the team`s score:\n");
-		scanf("%hi", &t[i].score);
+		scanf("%d", &t[i].score);
 	}
 	short max = 0;
 	short num = 0;
-	for (short i = 1; i < size; i++)
+	for (int i = 1; i < size; i++)
 	{
 		if (t[i].score > max)
 		{
@@ -34,15 +35,14 @@ void some_function(struct Sportteam* team)
 	}
 	for (short i = 0; i < size; i++)
 	{
-		printf("Team name: %s\n", t[i].name);
-		printf("City: %s\n", t[i].city);
-		printf("Score: %d\n", t[i].score);
+		printf("\nTeam name: %s\n", t[i].name);
+		printf("\nCity: %s\n", t[i].city);
+		printf("\nScore: %d\n", t[i].score);
 	}
 	printf("The team with the most points: %s", t[num].name);
 }
 
 void main()
 {
-	struct Sportteam* t;
 	some_function(NULL);
 }
